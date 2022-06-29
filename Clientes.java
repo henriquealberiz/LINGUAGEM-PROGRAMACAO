348bd18389a47235beb1286dc03d73c609b3746d
henriquealberiz <100207162+henriquealberiz@users.noreply.github.com>
2022-06-29 09:18:02 -0300

    Aplicação final da Unidade Complementar Linguagem de Programação

---
+++ Clientes.java
@@ -0,0 +1,58 @@
+
+package Modelo;
+
+/**
+ *
+ * @author Henrique
+ */
+public class Clientes {
+    String nome,cpf,endereço,titulo,inicio,fim;
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFim() {
+        return fim;
+    }
+
+    public void setFim(String fim) {
+        this.fim = fim;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+}
---
+++ Livros.java
@@ -0,0 +1,58 @@
+
+package Modelo;
+
+/**
+ *
+ * @author Henrique
+ */
+public class Livros {
+    String autor, título, código , preço, edição , quantidade ;
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTítulo() {
+        return título;
+    }
+
+    public void setTítulo(String título) {
+        this.título = título;
+    }
+
+    public String getCódigo() {
+        return código;
+    }
+
+    public void setCódigo(String código) {
+        this.código = código;
+    }
+
+    public String getPreço() {
+        return preço;
+    }
+
+    public void setPreço(String preço) {
+        this.preço = preço;
+    }
+
+    public String getEdição() {
+        return edição;
+    }
+
+    public void setEdição(String edição) {
+        this.edição = edição;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+}
